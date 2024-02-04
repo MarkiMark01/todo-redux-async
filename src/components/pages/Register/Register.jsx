3e3937ceea39ce106fad798a5a6f88e54f53a9ac
MarkiMark01 <markishgroup@gmail.com>
2024-02-04 15:22:29 +0200

    upgrade Register

--- src/components/pages/Register/Register.jsx
+++ src/components/pages/Register/Register.jsx
@@ -1,11 +1,42 @@
+import React, { useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <section>
       <form>
         <section>
-          <input type="text" placeholder="Your name" />
-          <input type="email" placeholder="Your email" />
-          <input type="password" placeholder="Your password" />
+          <input
+            type="text"
+            placeholder="Your name"
+            value={name}
+            onChange={handleName}
+          />
+          <input
+            type="email"
+            placeholder="Your email"
+            value={email}
+            onChange={handleEmail}
+          />
+          <input
+            type="password"
+            placeholder="Your password"
+            value={password}
+            onChange={handlePassword}
+          />
         </section>
         <section>
           <button type="button">Back</button>
