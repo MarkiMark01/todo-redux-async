9548a3b91c22119d71c9a05b52bf921881cd2e3f
MarkiMark01 <markishgroup@gmail.com>
2024-02-04 15:10:41 +0200

    upgrade Register, add inputs and buttons

--- src/components/pages/Register/Register.jsx
+++ src/components/pages/Register/Register.jsx
@@ -1,8 +1,19 @@
 const Register = () => {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <section>
+      <form>
+        <section>
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Your email" />
+          <input type="password" placeholder="Your password" />
+        </section>
+        <section>
+          <button type="button">Back</button>
+          <button type="button">Log In</button>
+          <button type="button">Register</button>
+        </section>
+      </form>
+    </section>
   );
 };
 export default Register;
