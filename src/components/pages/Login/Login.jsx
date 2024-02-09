38fca1f478fbbd620fa9d3ec377307a7d72b5ac6
MarkiMark01 <markishgroup@gmail.com>
2024-02-09 11:48:42 +0200

    upgrade Login

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,17 +1,37 @@
 import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <section>
       <form>
         <section>
-          <input type="email" placeholder="Your email" />
-          <input type="password" placeholder="Your password" />
+          <input
+            type="email"
+            placeholder="Your email"
+            value={email}
+            onChange={handleEmail}
+          />
+          <input
+            type="password"
+            placeholder="Your password"
+            value={password}
+            onChange={handlePassword}
+          />
         </section>
         <section>
           <button type="button">Back</button>
-          <button type="submit">Register</button>
-          <button type="button">Log In</button>
+          <button type="button">Register</button>
+          <button type="submit">Log In</button>
         </section>
       </form>
       {/* {status && <p style={{ color: "red" }}>{message}</p>} */}
