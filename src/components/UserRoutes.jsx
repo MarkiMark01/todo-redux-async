f81c0db34fca00eddc4682d892f4ab8893d5bf65
MarkiMark01 <markishgroup@gmail.com>
2024-02-10 17:12:17 +0200

    upgrade UserRoutes

--- src/components/UserRoutes.jsx
+++ src/components/UserRoutes.jsx
@@ -2,12 +2,14 @@ import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
 const Register = lazy(() => import("./pages/Register/Register"));
+const Login = lazy(() => import("./pages/Login/Login"));
 const Todo = lazy(() => import("./pages/Todo/Todo"));
 
 const UserRoutes = () => (
   <Suspense fallback={<p>Loading...</p>}>
     <Routes>
-      <Route path="/" element={<Register />} />
+      <Route path="/" element={<Login />} />
+      <Route path="register" element={<Register />} />
       <Route path="/todo" element={<Todo />} />
     </Routes>
   </Suspense>
