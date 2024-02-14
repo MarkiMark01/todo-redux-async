53641fce895f67e6b747551b8d63ab8c8da95cc9
MarkiMark01 <markishgroup@gmail.com>
2024-02-14 09:03:44 +0200

    upgrade Todo

--- src/components/pages/Todo/Todo.jsx
+++ src/components/pages/Todo/Todo.jsx
@@ -1,4 +1,17 @@
 const Todo = () => {
-  return <div>Todo</div>;
+  return (
+    <>
+      <header>
+        <section>
+          <span>Name</span>
+          <button>Log Out</button>
+        </section>
+      </header>
+      <main>
+        <p>Todo</p>
+      </main>
+      <footer>2024@Markiyan Marych</footer>
+    </>
+  );
 };
 export default Todo;
