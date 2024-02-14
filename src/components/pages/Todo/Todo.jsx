bc144b9247d695f1b1743848b763e9f8f7c2d21f
MarkiMark01 <markishgroup@gmail.com>
2024-02-14 09:13:39 +0200

    upgrade Todo

--- src/components/pages/Todo/Todo.jsx
+++ src/components/pages/Todo/Todo.jsx
@@ -1,14 +1,25 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { getUser } from "../../redux/auth/authSelectors";
+
 const Todo = () => {
+  const { name } = useSelector(getUser);
+
   return (
     <>
       <header>
+        <section>Filter</section>
         <section>
-          <span>Name</span>
+          <span>{name}</span>
           <button>Log Out</button>
         </section>
       </header>
       <main>
-        <p>Todo</p>
+        <section>
+          <p>enter tasks</p>
+        </section>
+        <section>Tasks List</section>
       </main>
       <footer>2024@Markiyan Marych</footer>
     </>
