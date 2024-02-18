3352f851029d7f4d1a069dd5f302be19858a1187
MarkiMark01 <markishgroup@gmail.com>
2024-02-18 16:31:48 +0200

    upgrade store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,8 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from './auth/authSlice';
+
+import authReducer from '../redux/auth/authSlice';
+import storage from "redux-persist/lib/storage";
+import {
+    persistReducer,
+    persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from "redux-persist";
+
+export const persistConfig = {
+    key: "token",
+    storage,
+    whitelist: ["token"]
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
-    }
+        auth: persistedReducer,
+
+
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
+
+export const persistor = persistStore(store); 
