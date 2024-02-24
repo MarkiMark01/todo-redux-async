20923da6c697e6ae19d8a511234e66f3377a4120
MarkiMark01 <markishgroup@gmail.com>
2024-02-24 11:58:14 +0200

    upgrade TodoList

--- src/components/pages/Todo/TodoList.jsx
+++ src/components/pages/Todo/TodoList.jsx
@@ -1,9 +1,14 @@
 import TodoItem from "./TodoItem";
+import { useSelector } from "react-redux";
 
 const TodoList = () => {
+  const todos = useSelector((state) => state.todos.todos);
+
   return (
     <ul>
-      <TodoItem />
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} {...todo} />
+      ))}
     </ul>
   );
 };
