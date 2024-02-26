bbd75103b951fb54ad66a688502e915f375cdb78
MarkiMark01 <markishgroup@gmail.com>
2024-02-26 13:29:57 +0200

    update NewTasks

--- src/components/pages/Todo/NewTasks.jsx
+++ src/components/pages/Todo/NewTasks.jsx
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { addTodos } from "../../redux/todo/todoOperations";
 
 const NewTasks = () => {
   const [text, setText] = useState("");
 
+  const dispatch = useDispatch();
+
   const handleText = (e) => {
     setText(e.target.value);
   };
+
+  const isDuplicate = () => {
+    return new Set(text.map((item) => item.text)).size !== text.length;
+  };
+
+  const handleAction = () => {
+    if (text.trim().length && !isDuplicate()) {
+      dispatch(addTodos(text));
+      setText("");
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleAction}>
       <input
         value={text}
         onChange={handleText}
         placeholder="Enter your tasks"
       ></input>
-      <button type="button">Add Tasks</button>
+      <button type="submit">Add Tasks</button>
     </form>
   );
 };
