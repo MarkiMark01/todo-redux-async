7d8e0be2d915bab236967acf79c6d66abe0fc572
markmarych <markishcorp@gmail.com>
2024-03-01 14:49:50 +0200

    update Login, Header, footer scss

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,6 +1,8 @@
+import styles from "../shared/styles/styles.module.scss";
+
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <span>2024@Markiyan Marych</span>
     </footer>
   );
